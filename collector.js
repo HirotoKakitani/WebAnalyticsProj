6a4067f59bfeea49cca019efe5af1c8767741afc
Hiroto Kakitani <hirotokakitani@gmail.com>
2019-03-02 13:25:50 -0800

    collector script

--- collector.js
+++ collector.js
@@ -4,6 +4,20 @@
 //TODO generate report
 //TODO authentication
 
+//sessionization
+if (document.cookie.split(';').filter((item) => item.trim().startsWith('sessionId=')).length) {
+    //check if sessionId cookie is set
+    var match = document.cookie.match(new RegExp('(^| )' + 'sessionId' + '=([^;]+)'));
+    if (match)
+        var sessionId = match[2];
+    console.log('The cookie exists');
+}
+//issue a cookie
+else{
+    var sessionId = Math.random().toString(36).substr(2,16);
+    document.cookie = "sessionId="+sessionId;
+    console.log (sessionId);
+}
 //records static information
 window.addEventListener('load',function(){
     console.log("Collector script loaded!");
@@ -22,7 +36,17 @@ window.addEventListener('load',function(){
     //console.log("Total page load time : " + totalLoadTime);
     console.log("Connect time: " + connectTime);
     console.log("Render time: " + renderTime);
+    
+    console.log("load session: " + sessionId);
 
+    //TODO beacon does not seem to fire when FromData being sent
+    var url = "http://localhost:8000/api/log.php";
+    var data = new FormData();
+    data.append('test', 'Hello!');
+    //var data = "test=testing!";
+    //console.log(data);
+    var res = navigator.sendBeacon(url,data);
+    console.log("res: " + res);
 });
 
 //records errors
@@ -30,6 +54,8 @@ window.addEventListener('error',function(event){
     var errorMessage = event.message;
     var errorTimeStamp = event.timeStamp;
     console.log("Error: " + errorMessage + " at time " + errorTimeStamp);    
+    
+    console.log("error session: " + sessionId);
 });
 
 //records click positions and element clicked
@@ -44,6 +70,6 @@ window.addEventListener('click',function(event){
 window.addEventListener('scroll', function(event){
     var scrollTarget = event.target.id || event.target.activeElement.tagName;
     var scrollTime = event.timeStamp;
-    console.log("Scrolled " + scrollTarget + " at " + scrollTime);
+    //console.log("Scrolled " + scrollTarget + " at " + scrollTime);
     
 },true);
